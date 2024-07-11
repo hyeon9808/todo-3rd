4de701b0dbd0bdc6588b45d57a3b2e88cb84f3fa
hyeon9808 <hyeon9808@gmail.com>
2024-07-11 13:26:10 +0900

    숙련주차 시험

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { getTodoByID } from "../redux/modules/todos.js";
+
+const Detail = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos.todos); // todos 배열 전체를 가져옴
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getTodoByID(id));
+  }, [dispatch, id]);
+
+  // 특정 ID와 완료 상태인 todo를 찾음
+  const todo = todos.find((todo) => todo.id === id && todo.isDone === true);
+
+  // 취소 버튼 클릭 시 이전 페이지로 이동하는 함수
+  const handleCancel = () => {
+    navigate("/"); // 이전으로 돌아가는 경로 설정
+  };
+
+  return (
+    <StContainer>
+      <StDialog>
+        <div>
+          <StDialogHeader>
+            <div>ID :{todo.id}</div>
+            <StButton
+              borderColor="#ddd"
+              onClick={handleCancel} // 취소 버튼 클릭 시 handleCancel 함수 호출
+            >
+              이전으로
+            </StButton>
+          </StDialogHeader>
+          <StTitle>{todo.title}</StTitle>
+          <StBody>{todo.body}</StBody>
+        </div>
+      </StDialog>
+    </StContainer>
+  );
+};
+
+export default Detail;
+
+const StContainer = styled.div`
+  border: 2px solid #eee;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StDialog = styled.div`
+  width: 600px;
+  height: 400px;
+  border: 1px solid #eee;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const StDialogHeader = styled.div`
+  display: flex;
+  height: 80px;
+  justify-content: space-between;
+  padding: 0 24px;
+  align-items: center;
+`;
+
+const StTitle = styled.h1`
+  padding: 0 24px;
+`;
+
+const StBody = styled.main`
+  padding: 0 24px;
+`;
+
+const StButton = styled.button`
+  border: 1px solid ${({ borderColor }) => borderColor};
+  height: 40px;
+  width: 120px;
+  background-color: #fff;
+  border-radius: 12px;
+  cursor: pointer;
+`;
